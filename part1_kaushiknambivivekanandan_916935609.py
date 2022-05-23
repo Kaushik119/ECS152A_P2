d3780be376e9333ca2f2a8f81b536c57a37d25dc
Prabhdeep Kainth <pdkainth@gmail.com>
2022-05-23 14:59:52 -0700

    part 1 done

--- part1_kaushiknambivivekanandan_916935609.py
+++ part1_kaushiknambivivekanandan_916935609.py
@@ -1,8 +1,10 @@
 import socket
 import os
 import time
+import math
 
 read_from_file_size = 949
+BufferSize = 100
 
 if __name__ == "__main__":
   message = open("message.txt", 'r')
@@ -14,12 +16,50 @@ if __name__ == "__main__":
   Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   Socket.connect(Server)
 
-  test_packet = packet.read(read_from_file_size = 949)
-  packet = "1|" + packet
+  cur_file_size = 0
+  packet_num = 1
+  create_new_packet = True
+  test_packet = ""
+  delays = []
 
-  encoded_packet = str.encode(packet)
+  while(cur_file_size < file_size):
+    if(create_new_packet):
+      test_packet = message.read(read_from_file_size)
+      test_packet = str(packet_num) + "|" + test_packet
+      start = time.time()
 
-  Socket.send(encoded_packet)
+    encoded_packet = str.encode(test_packet)
 
-  response = Socket.recv(BufferSize).decode()
-  print(response)
+    Socket.settimeout(5)
+
+    try:
+      Socket.send(encoded_packet)
+
+      response = Socket.recv(BufferSize).decode()
+      end = time.time()
+      delays.append(end - start)
+
+      print()
+      print("Current Window: [" + str(packet_num) + "]")
+      print("Sequence Number of Packet Sent: " + str(packet_num))
+      print("Acknowledgement Number Received: " + str(packet_num))
+      print()
+
+      packet_num += 1
+      cur_file_size += 1000
+      create_new_packet = True
+    except socket.timeout as ex:
+      create_test_packet = False
+  
+  delay_avg = sum(delays) / len(delays)
+  throughput_avg = cur_file_size / sum(delays)
+
+
+  print("Average Delay = <" + str(delay_avg) + ">")
+  print("Average Throughput = <" + str(throughput_avg) + ">")
+  print("Performance = <" + str(math.log(throughput_avg, 10) - math.log(delay_avg, 10)) + ">")
+
+      
+  
+
+
