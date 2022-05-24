c4a4731f494a7eb8e1f02188f0e951b889178076
Kaushik119 <kaushiknambi@gmail.com>
2022-05-23 20:59:10 -0400

    some changes to part 3

--- part3_kaushiknambivivekanandan_916935609.py
+++ part3_kaushiknambivivekanandan_916935609.py
@@ -43,22 +43,34 @@ def main():
   
   start_time = 0
   end_time = 0
+  first_packet = True
+  
+  # While there is still stuff left in the file to send
   while(start_at < file_size):
+    # Send packets in the current congestion window
     for i in range(0 ,cwnd):
+      # Find the send time of the last packet in the congestion window
       start_time = time.time()
+      # Read from the file
       packet = message.read(read_from_file_size)
+      # Update offset
       start_at += len(packet)
+      # Adding the header to the packets
       packet = str(message_counter) + "|" + packet
       message_counter += 1
       past_packets.append(packet)
 
       encoded_packet = str.encode(packet)
 
+      # Sending the packet
       Socket.send(encoded_packet)
       
+    # Storing the window size for the packets we sent
     current_cwnd = cwnd
+    # Counter for how many packets we received
     recv_counter = 0
     
+    # While all the packets haven't been received
     while recv_counter < current_cwnd:
       Socket.settimeout(timeout_seconds)
       try:
@@ -75,15 +87,24 @@ def main():
             in_congestion = True
 
       except socket.timeout:
-        Socket.send(str.encode(past_packets[response+1]))
+        Socket.send(str.encode(past_packets[int(response)+1]))
         ssthresh = int(cwnd/2)
         cwnd = 1
         in_congestion = False
 
+    print(timeout_seconds)
+    
     SampleRTT = end_time - start_time
+    
+    if first_packet:
+      EstimatedRTT = SampleRTT
+      first_packet = False
+    
     EstimatedRTT = alpha_1*EstimatedRTT + alpha*(SampleRTT)
     DevRTT = beta_1*DevRTT + beta*(SampleRTT- EstimatedRTT)
-    timeout_seconds = EstimatedRTT+4*DevRTT
+    timeout_seconds = abs(EstimatedRTT+4*DevRTT)
+    print(timeout_seconds)
+    print()
 
 
 if __name__ == "__main__":
