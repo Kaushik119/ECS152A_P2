62391b2a084eca01e52912564c1d98323fe06cd1
Kaushik119 <kaushiknambi@gmail.com>
2022-05-24 01:50:30 -0400

    part3 modified

--- part3_kaushiknambivivekanandan_916935609.py
+++ part3_kaushiknambivivekanandan_916935609.py
@@ -1,101 +1,139 @@
+# Importing libraries
 import socket
 import os
 import time
 
-
-# TCP TAHOE IMPLEMENTED AS DESCRIBED IN TEXTBOOK
-
-read_from_file_size = 949
+# Global variables
+read_from_file_size = 1000
 timeout_seconds = 5
 cwnd = 1
 ssthresh = 16
 BufferSize = 100
+start_index = 1
 
+# Function that tells us if we are in slow start
 def is_slow_start():
   return cwnd <= ssthresh
 
+# Function that tells us if we received a response for the left most item in the window
+def left_hand_response(response):
+  return response == start_index
+
 def main():
+  # Open the message
   message = open("message.txt", 'r')
   file_size = os.stat("message.txt").st_size
   start_at = 0
 
+  # Connect to the socket
   port_number = int(input("Enter port number: "))
   Server = ("", port_number)
   
   Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   Socket.connect(Server)
   
+  # Variables to keep track of packet and packet count 
   packet = ""
-  past_packets = []
+  packets = []
   message_counter = 1
   in_congestion = False
   
+  # Bringing global variables in scope
   global ssthresh
   global cwnd
   global timeout_seconds
+  global start_index
   
+  # Constants for calculating RTT based timeout
   EstimatedRTT = 0
   DevRTT = 0
   alpha = 0.875
   alpha_1 = 0.125
   beta = 0.25
   beta_1 = 0.75
-  
-  start_time = 0
-  end_time = 0
+
+  # Flag for first packet timeout calculations
   first_packet = True
   
-  # While there is still stuff left in the file to send
-  while(start_at < file_size):
-    # Send packets in the current congestion window
-    for i in range(0 ,cwnd):
-      # Find the send time of the last packet in the congestion window
-      start_time = time.time()
-      # Read from the file
-      packet = message.read(read_from_file_size)
-      # Update offset
-      start_at += len(packet)
-      # Adding the header to the packets
-      packet = str(message_counter) + "|" + packet
-      message_counter += 1
-      past_packets.append(packet)
+  # Creating all the packets to send
+  while start_at < file_size:
+    packet = message.read(read_from_file_size)
+    start_at += len(packet)
+    # Adding the header to the packets
+    packet = str(message_counter) + "|" + packet
+    message_counter += 1
+    packets.append(packet)
 
-      encoded_packet = str.encode(packet)
+  # Counter to keep track of counter
+  packet_count = 0
+  # Time at which we send the packet
+  BeginTimes = []
+  # Round Trip Times for each packet
+  RTTTimes = [0] * len(packets)
+  # Last packet that we set the RTT for
+  RTTLast = 0
 
-      # Sending the packet
-      Socket.send(encoded_packet)
+  # Send the first packet
+  Socket.send(str.encode(packets[packet_count]))
+  BeginTimes.append(time.time)
+  packet_count+=1
+
+  # While packets still left to send
+  while packet_count < len(packets):
+    # Start timeout
+    Socket.settimeout(timeout_seconds)
+    try:
+      # receive response
+      response = Socket.recv(BufferSize).decode()
+      # Check end time
+      end_time = time.time()
       
-    # Storing the window size for the packets we sent
-    current_cwnd = cwnd
-    # Counter for how many packets we received
-    recv_counter = 0
+      # set RTTs for all the packets sent until the one we got the response for
+      while RTTLast < response:
+        RTTTimes[RTTLast] = BeginTimes[RTTLast] - end_time
+        RTTLast += 1
+
+      # If slow start
+      if is_slow_start():
+        # Increase it per receive
+        cwnd += 1
+
+        # If the response was for a left most packet in the window
+        if left_hand_response(response):
+          start_index += 1
+          # We can slide the window by 1 and send the packets in the new congestion window
+          for i in range(packet_count, packet_count+cwnd):
+            Socket.send(str.encode(packets[i]))
+            BeginTimes.append(time.time)
+            packet_count += 1
+            in_congestion = False
     
-    # While all the packets haven't been received
-    while recv_counter < current_cwnd:
-      Socket.settimeout(timeout_seconds)
-      try:
-        response = Socket.recv(BufferSize).decode()
-        end_time = time.time()
-        recv_counter += 1
-        if is_slow_start():
+      # If not slow start
+      else:
+        # do congestion control
+        # We only want to do it once per RTT
+        if in_congestion == False:
           cwnd += 1
-          in_congestion = False
-        else:
-          # do congestion control
-          if in_congestion == False:
-            cwnd += 1
-            in_congestion = True
+          start_index += 1
+          in_congestion = True
 
-      except socket.timeout:
-        Socket.send(str.encode(past_packets[int(response)+1]))
-        ssthresh = int(cwnd/2)
-        cwnd = 1
-        in_congestion = False
+        if left_hand_response(response):
+          start_index += 1
+          for i in range(packet_count, packet_count+cwnd):
+            Socket.send(str.encode(packets[i]))
+            packet_count += 1
+            in_congestion = False
+  
+    except socket.timeout:
+      Socket.send(str.encode(packets[int(response)+1]))
+      ssthresh = int(cwnd/2)
+      cwnd = 1
+      in_congestion = False
 
-    print(timeout_seconds)
-    
-    SampleRTT = end_time - start_time
-    
+    in_congestion = False
+
+    SampleRTT = RTTTimes[RTTLast]
+
     if first_packet:
       EstimatedRTT = SampleRTT
       first_packet = False
@@ -103,9 +141,3 @@ def main():
     EstimatedRTT = alpha_1*EstimatedRTT + alpha*(SampleRTT)
     DevRTT = beta_1*DevRTT + beta*(SampleRTT- EstimatedRTT)
     timeout_seconds = abs(EstimatedRTT+4*DevRTT)
-    print(timeout_seconds)
-    print()
-
-
-if __name__ == "__main__":
-  main()
