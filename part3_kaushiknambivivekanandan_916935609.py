118dd1fe07290cb9b90ba4a189a721eb9baf534e
Kaushik119 <kaushiknambi@gmail.com>
2022-05-24 22:19:04 -0400

    part3 over

--- part3_kaushiknambivivekanandan_916935609.py
+++ part3_kaushiknambivivekanandan_916935609.py
@@ -14,6 +14,9 @@ start_index = 1
 def is_slow_start():
   return cwnd <= ssthresh
 
+def left_hand_response(response):
+  return response == start_index
+
 def main():
   message = open("/home/osboxes/Desktop/ECS152A/ECS152A_P2/message.txt", 'r')
   file_size = os.stat("/home/osboxes/Desktop/ECS152A/ECS152A_P2/message.txt").st_size
@@ -44,6 +47,7 @@ def main():
   
   start_time = 0
   end_time = 0
+  prev_response = 0
 
   BeginTimes = []
 
@@ -58,28 +62,40 @@ def main():
   packet_count = 0
   RTTTimes = [0]*len(packets)
   RTTLast = 0
+  sent_counter = 0
+  ack_array = [False]*len(packets)
 
   while packet_count < len(packets):
-    for i in range(0 ,min(len(packets) - packet_count,cwnd)):
+    for i in range(sent_counter ,min(len(packets) - packet_count,cwnd)):
       Socket.send(str.encode(packets[packet_count]))
       BeginTimes.append(time.time())
-      print("Packet count" ,packet_count + 1)
       packet_count += 1
-
+      
     current_cwnd = min(len(packets) - packet_count,cwnd)
     recv_counter = 0
-
+    sent_counter = 0
     while recv_counter < current_cwnd:
       Socket.settimeout(timeout_seconds)
       try:
         response = int(Socket.recv(BufferSize).decode())
-        print("Response",response)
-        end_time = time.time()
-        recv_counter += 1
+        recv_counter += (response - prev_response)
+        prev_response = response
 
+        Socket.settimeout(None)
+
+        end_time = time.time()
+        
         while RTTLast < response:
           RTTTimes[RTTLast] = end_time - BeginTimes[RTTLast]
           RTTLast += 1
+        ack_array[0:response] = [True]*(response)
+ 
+        if left_hand_response(response):
+          start_index = ack_array.index(False) + 1
+          Socket.send(str.encode(packets[packet_count]))
+          sent_counter+=1
+          BeginTimes.append(time.time())
+          packet_count += 1
 
         if is_slow_start():
           cwnd += 1
@@ -91,17 +107,18 @@ def main():
             in_congestion = True
 
       except socket.timeout:
-        print("Timed out, send packet" ,response + 1)
         Socket.send(str.encode(packets[response]))
         ssthresh = int(cwnd/2)
         cwnd = 1
         in_congestion = False
+    
 
-      in_congestion = False
-      SampleRTT = RTTTimes[RTTLast-1]
-      EstimatedRTT = alpha_1*EstimatedRTT + alpha*(SampleRTT)
-      DevRTT = beta_1*DevRTT + beta*abs(SampleRTT- EstimatedRTT)
-      timeout_seconds = EstimatedRTT+4*DevRTT
+    in_congestion = False
+    SampleRTT = RTTTimes[RTTLast-1]
+    EstimatedRTT = alpha_1*EstimatedRTT + alpha*(SampleRTT)
+    DevRTT = beta_1*DevRTT + beta*(SampleRTT- EstimatedRTT)
+    timeout_seconds = EstimatedRTT+4*DevRTT
+
 
 if __name__ == "__main__":
   main()
