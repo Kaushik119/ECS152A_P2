b59774fda7ec1ecb70efb2c8bfde352114a87c87
Prabhdeep Kainth <pdkainth@gmail.com>
2022-05-26 19:25:49 -0700

    plots

--- part1_kaushiknambivivekanandan_916935609.py
+++ part1_kaushiknambivivekanandan_916935609.py
@@ -2,6 +2,9 @@ import socket
 import os
 import time
 import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
 
 read_from_file_size = 1000
 BufferSize = 100
@@ -58,6 +61,27 @@ if __name__ == "__main__":
   print("Average Delay = <" + str(delay_avg) + ">")
   print("Average Throughput = <" + str(throughput_avg) + ">")
   print("Performance = <" + str(math.log(throughput_avg, 10) - math.log(delay_avg, 10)) + ">")
+
+  packet_size = sys.getsizeof(test_packet) * 8
+  ppt = []
+  for delay in delays:
+    ppt.append(packet_size / delay)
+
+  
+  RTT = sns.lineplot(x = range(0, len(delays)),y = delays)
+  RTT.set(xlabel = "Packet Number", ylabel = "RTT", title = "RTT for Packets on Port " + str(port_number))
+
+  image_name = "Part1_RTT"+str(port_number)+".png"
+  plt.savefig("Images/"+image_name)
+
+  plt.clf()
+
+  Throughput = sns.lineplot(x = range(0, len(ppt)),y = ppt)
+  Throughput.set(xlabel = "Packet Number", ylabel = "Per Packet Throughput", title = "PPT for Packets on Port " + str(port_number))
+
+  image_name = "Part1_PPT"+str(port_number)+".png"
+  plt.savefig("Images/"+image_name)
+
 
       
   
